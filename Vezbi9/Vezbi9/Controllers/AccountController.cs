8b14734311f6a22fd9bbe1b8bdc9309b038b1625
Mihail <siljanovski.mihail@uklo.edu.mk>
2023-04-26 21:32:36 +0200

    Vezbi9

---
+++ Vezbi9/Vezbi9/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vezbi9.Models;
+
+namespace Vezbi9.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Authenticate user
+                if (model.Username == "admin" && model.Password == "password")
+                {
+                  
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Vezbi9/Vezbi9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vezbi9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
